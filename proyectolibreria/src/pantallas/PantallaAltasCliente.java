3b673bdf6995db6518fd07cccb22e47d50bb0d68
MIKE <MIKE@MIKE-HP>
2020-11-10 18:15:44 +0100

    subo 18:15

--- proyectolibreria/src/pantallas/PantallaAltasCliente.java
+++ proyectolibreria/src/pantallas/PantallaAltasCliente.java
@@ -1,198 +1,172 @@
 package pantallas;
-/** 
-*/
-import javax.swing.*;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.table.*;
-
-import java.sql.ResultSet;
 import java.util.*;
-
 import javax.swing.event.*;
 
 import proyecto.Clientes;
 import proyecto.ConsultaClientes;
+
 public class  PantallaAltasCliente extends JPanel implements ActionListener, TableModelListener{
-public static int c=0;	
-//public static JFrame frame;
-public JTable table;
 
-public DefaultTableModel tabModel;
-public JScrollPane scrollPane;
-public JLabel lblbienvenida,lblId,lblNombre,lblPrimerApellido,lblSegundoApellido,lblDireccion,lblTelefono,lblCifnif,lblPoblacion,lblCp,lblSexo,lblMovil,lblFax,lblEmail,lblNcuenta,lblFechanacimiento,lblFechainicio,lblComentarios;
-public JTextField txtId,txtNombre,txtPrimerApellido,txtSegundoApellido,txtDireccion,txtTelefono,txtCifnif,txtPoblacion,txtCp,txtSexo,txtMovil,txtFax,txtEmail,txtNcuenta,txtFechanacimiento,txtFechainicio,txtComentarios;
-public JButton cmdAdd,cmdDelete,cmdSetValue,cmdExit,cmdChange;
-public JPanel mainPanel;
-public ResultSet resulta;
+	public JPanel mainPanel;
+	
+	public JTable table;
+	public DefaultTableModel tabModel;
+	
+	public JScrollPane scrollPane;
+	
+	public JLabel lblbienvenida,lblId,lblNombre,lblPrimerApellido,lblSegundoApellido,lblDireccion,lblTelefono,lblCifnif,lblPoblacion,lblCp,lblSexo,lblMovil,lblFax,lblEmail,lblNcuenta,lblFechanacimiento,lblFechainicio,lblComentarios;
+	public JTextField txtId,txtNombre,txtPrimerApellido,txtSegundoApellido,txtDireccion,txtTelefono,txtCifnif,txtPoblacion,txtCp,txtSexo,txtMovil,txtFax,txtEmail,txtNcuenta,txtFechanacimiento,txtFechainicio,txtComentarios;
+	
+	public JButton cmdAdd,cmdDelete,cmdSetValue,cmdExit,cmdChange;
+	
 
-private Vector columns;
-private Vector rows;
-public PantallaAltasCliente(){	 
-	   
-	 	setLayout( new BorderLayout() );   
-	 	columns= new Vector();
-	 	rows= new Vector();
-		String[] columnNames ={"ID",
-				"Nombre",
-				"Primer apellido",
-				"Segundo apellido",
-				"Cif/Nif",
-				"Direccion",
-				"Poblacion",
-				"CP",
-				"SEXO",
-				"Telefono",
-				"Movil",
-				"Fax",
-				"Email",
-				"Ncuenta",
-				"Fecha nacimiento",
-				"Fecha alta",
-				"Comentarios"};
+	private Vector columns;
+	private Vector rows;
+
+	public PantallaAltasCliente(){	 
+		   
+		 	setLayout( new BorderLayout() );   
+		 	
+		 	columns= new Vector();
+		 	rows= new Vector();
+
+			String[] columnNames ={
+					"ID",
+					"Nombre",
+					"Primer apellido",
+					"Segundo apellido",
+					"Cif/Nif",
+					"Direccion",
+					"Poblacion",
+					"CP",
+					"SEXO",
+					"Telefono",
+					"Movil",
+					"Fax",
+					"Email",
+					"Ncuenta",
+					"Fecha nacimiento",
+					"Fecha alta",
+					"Comentarios"};
+			
+			addColumns(columnNames);	
+			
+			Vector<Clientes> filas= ConsultaClientes.consulta();
+			
+			tabModel=new DefaultTableModel();
+			tabModel.setDataVector(rows,columns);
+						
+			for (Clientes cliente : filas) 
+			{		
+	            Object[] fila = new Object[17];//Creamos un Objeto con tantos parámetros como datos retorne cada fila de la consulta
+	            fila[0]  = cliente.getId();
+	            fila[1]  = cliente.getNombre(); //Lo que hay entre comillas son los campos de la base de datos
+	            fila[2]  = cliente.getApellido1();
+	            fila[3]  = cliente.getApellido2();
+	            fila[4]  = cliente.getCifNif();
+	            fila[5]  = cliente.getDireccion();
+	            fila[6]  = cliente.getPoblacion();
+	            fila[7]  = cliente.getCp();
+	            fila[8]  = cliente.getSexo();           
+	            fila[9]  = cliente.getTelefono();
+	            fila[10] = cliente.getMovil();
+	            fila[11] = cliente.getFax();
+	            fila[12] = cliente.getEmail();
+	            fila[13] = cliente.getNcuenta();
+	            fila[14] = cliente.getFechaNacimiento();
+	            fila[15] = cliente.getFechaAlta();
+	            fila[16] = cliente.getComentarios();
+	            tabModel.addRow(fila); // Añade una fila al final del modelo de la tabla			
+			}
+			//*table.updateUI();//Actualiza la tabla
+			
+			//scrollPane.setViewportView(table);//Esto añade la tabla al portView del scrollPane, si estaba puesto anteriormente
+	        //hay que borrarlo del otro sitio, sino puede dar error de NullPointerException
+			
+			mainPanel=new JPanel();
+			
+			table = new JTable(tabModel);
+			
+			scrollPane= new JScrollPane(table);
+			
+			mainPanel.setLayout(null);
+			mainPanel.add(scrollPane);
+			
+			scrollPane.setBounds(10,50,500,300);
+			scrollPane.setSize(1250, 200);
+			
+			table.setRowSelectionAllowed(true);
+			table.getModel().addTableModelListener(this);
+			table.setSize(1250, 400);
+			
+			lblbienvenida=new JLabel("ALTA CLIENTES");lblbienvenida.setBounds(600, 10, 100, 30);mainPanel.add(lblbienvenida);
+	
+	cmdDelete=new JButton("Borrar");					cmdDelete.setBounds(300, 350,100 ,30);				mainPanel.add(cmdDelete);
+	cmdSetValue=new JButton("Añadir");					cmdSetValue.setBounds(500, 350,100 ,30);			mainPanel.add(cmdSetValue);
+	cmdExit=new JButton("Salir");						cmdExit.setBounds(700, 350,120 ,30);				mainPanel.add(cmdExit);
+	cmdChange=new JButton("Modificar");					cmdChange.setBounds(900, 350,100 ,30);				mainPanel.add(cmdChange);
+	
+	lblId=new JLabel("ID:");							lblId.setBounds(60, 450, 120, 30);					mainPanel.add(lblId);
+	txtId= new JTextField(5);							txtId.setBounds(160, 450, 120, 25);					mainPanel.add(txtId);
+	lblNombre=new JLabel("Nombre:");					lblNombre.setBounds(300, 450, 120, 30);				mainPanel.add(lblNombre);
+	txtNombre= new JTextField( 5 );						txtNombre.setBounds(410, 450, 120, 25);				mainPanel.add(txtNombre);
+	lblPrimerApellido=new JLabel("Primer Apellido:");	lblPrimerApellido.setBounds(550, 450, 120, 30);		mainPanel.add(lblPrimerApellido);
+	txtPrimerApellido=new JTextField(5);				txtPrimerApellido.setBounds(650, 450, 120, 25);		mainPanel.add(txtPrimerApellido);
+	lblSegundoApellido=new JLabel("Segundo Apellido:");	lblSegundoApellido.setBounds(800, 450, 120, 30);	mainPanel.add(lblSegundoApellido);
+	txtSegundoApellido=new JTextField(5);				txtSegundoApellido.setBounds(900, 450, 120, 25);	mainPanel.add(txtSegundoApellido);
+	lblCifnif=new JLabel("Nif:");	                    lblCifnif.setBounds(1050, 450, 120, 30);            mainPanel.add(lblCifnif);
+	txtCifnif=new JTextField(5);				        txtCifnif.setBounds(1100, 450, 120, 25);            mainPanel.add(txtCifnif);
+	
+	lblDireccion=new JLabel("Direccion:");				lblDireccion.setBounds(60, 500, 120, 30);			mainPanel.add(lblDireccion);
+	txtDireccion=new JTextField(5);						txtDireccion.setBounds(160, 500, 120, 25);			mainPanel.add(txtDireccion);
+	lblPoblacion=new JLabel("Poblacion:");				lblPoblacion.setBounds(300, 500, 120, 30);			mainPanel.add(lblPoblacion);
+	txtPoblacion=new JTextField(5);						txtPoblacion.setBounds(410, 500, 120, 25);			mainPanel.add(txtPoblacion);
+	lblCp=new JLabel("Cp:");							lblCp.setBounds(550, 500, 120, 30);					mainPanel.add(lblCp);
+	txtCp=new JTextField(5);							txtCp.setBounds(650,500, 120, 25);					mainPanel.add(txtCp);
+	lblSexo=new JLabel("Sexo:");						lblSexo.setBounds(800, 500, 120, 30);				mainPanel.add(lblSexo);
+	txtSexo=new JTextField(5);							txtSexo.setBounds(900, 500, 120, 25);				mainPanel.add(txtSexo);
+	lblTelefono=new JLabel("Telefono:");				lblTelefono.setBounds(1050, 500, 120, 30);			mainPanel.add(lblTelefono);
+	txtTelefono=new JTextField(5);						txtTelefono.setBounds(1100, 500, 120, 25);			mainPanel.add(txtTelefono);
+	
+	lblMovil=new JLabel("Movil:");						lblMovil.setBounds(60, 550, 120, 30);				mainPanel.add(lblMovil);
+	txtMovil=new JTextField(5);							txtMovil.setBounds(160, 550, 120, 25);				mainPanel.add(txtMovil);
+	lblFax=new JLabel("Fax:");			   				lblFax.setBounds(300, 550, 120, 30);				mainPanel.add(lblFax);
+	txtFax=new JTextField(5);							txtFax.setBounds(410, 550, 120, 25);				mainPanel.add(txtFax);
+	lblEmail=new JLabel("Email:");			   			lblEmail.setBounds(550, 550, 120, 30);				mainPanel.add(lblEmail);
+	txtEmail=new JTextField(5);							txtEmail.setBounds(650, 550, 120, 25);				mainPanel.add(txtEmail);
+	lblNcuenta=new JLabel("Ncuenta:");			   		lblNcuenta.setBounds(800, 550, 120, 30);			mainPanel.add(lblNcuenta);
+	txtNcuenta=new JTextField(5);						txtNcuenta.setBounds(900, 550, 120, 25);			mainPanel.add(txtNcuenta);
+	lblFechanacimiento=new JLabel("F.nac.:");	        lblFechanacimiento.setBounds(1050, 550, 120, 30);	mainPanel.add(lblFechanacimiento);
+	txtFechanacimiento=new JTextField(5);				txtFechanacimiento.setBounds(1100, 550, 120, 25);	mainPanel.add(txtFechanacimiento);
+	
+	lblFechainicio=new JLabel("Fechainicio:");			lblFechainicio.setBounds(60, 600, 120, 30);		    mainPanel.add(lblFechainicio);
+	txtFechainicio=new JTextField(5);					txtFechainicio.setBounds(160, 600, 120, 25);		mainPanel.add(txtFechainicio);
+	lblComentarios=new JLabel("Comentarios:");			lblComentarios.setBounds(300, 600, 120, 30);		mainPanel.add(lblComentarios);
+	txtComentarios=new JTextField(5);					txtComentarios.setBounds(410, 600, 120, 25);		mainPanel.add(txtComentarios);
+	
+	cmdDelete.addActionListener(this);
+	cmdSetValue.addActionListener(this);
+	cmdExit.addActionListener(this);
+	cmdChange.addActionListener(this);
 		
-		addColumns(columnNames);	
-		
-		Vector<Clientes> filas= ConsultaClientes.consulta();
-		
-		tabModel=new DefaultTableModel();
-		tabModel.setDataVector(rows,columns);
-		
-		for (Clientes cliente : filas) 
-		{		
-            Object[] fila = new Object[17];//Creamos un Objeto con tantos parámetros como datos retorne cada fila de la consulta
-            fila[0] = cliente.getId();
-            fila[1] = cliente.getNombre(); //Lo que hay entre comillas son los campos de la base de datos
-            fila[2] = cliente.getApellido1();
-            fila[3] = cliente.getApellido2();
-            fila[4] = cliente.getCifNif();
-            fila[5] = cliente.getDireccion();
-            fila[6] = cliente.getPoblacion();
-            fila[7] = cliente.getCp();
-            fila[8] = cliente.getSexo();           
-            fila[9] = cliente.getTelefono();
-            fila[10] = cliente.getMovil();
-            fila[11] = cliente.getFax();
-            fila[12] = cliente.getEmail();
-            fila[13] = cliente.getNcuenta();
-            fila[14] = cliente.getFechaNacimiento();
-            fila[15] = cliente.getFechaAlta();
-            fila[16] = cliente.getComentarios();
-            tabModel.addRow(fila); // Añade una fila al final del modelo de la tabla			
-		}
-		/*table.updateUI();//Actualiza la tabla
-		/*
-		resulta=ConsultaClientes.resultado();
-		addcontent(resulta);
-		*/
-		
-		//scrollPane.setViewportView(table);//Esto añade la tabla al portView del scrollPane, si estaba puesto anteriormente
-        //hay que borrarlo del otro sitio, sino puede dar error de NullPointerException
-				
-		table = new JTable(tabModel);
-		mainPanel=new JPanel();
-		scrollPane= new JScrollPane(table);//ScrollPane
-		mainPanel.setLayout(null);
-		mainPanel.add(scrollPane);
-		scrollPane.setBounds(50,50,500,300);
-		scrollPane.setSize(1250, 200);
-		table.setRowSelectionAllowed(true);
-		table.getModel().addTableModelListener(this);
-		table.setSize(1250, 400);
-		lblbienvenida=new JLabel("ALTA CLIENTES");lblbienvenida.setBounds(600, 10, 100, 30);mainPanel.add(lblbienvenida);
+	mainPanel.setBackground(Color.white);
+	
+	table.getParent().setBackground(Color.lightGray);
 
-cmdDelete=new JButton("Borrar");					cmdDelete.setBounds(300, 350,100 ,30);				mainPanel.add(cmdDelete);
-cmdSetValue=new JButton("Añadir");					cmdSetValue.setBounds(500, 350,100 ,30);			mainPanel.add(cmdSetValue);
-cmdExit=new JButton("Salir");						cmdExit.setBounds(700, 350,120 ,30);				mainPanel.add(cmdExit);
-cmdChange=new JButton("Modificar");					cmdChange.setBounds(900, 350,100 ,30);				mainPanel.add(cmdChange);
+	}
 
-lblId=new JLabel("ID:");							lblId.setBounds(60, 450, 120, 30);					mainPanel.add(lblId);
-txtId= new JTextField(5);							txtId.setBounds(160, 450, 120, 25);					mainPanel.add(txtId);
-lblNombre=new JLabel("Nombre:");					lblNombre.setBounds(300, 450, 120, 30);				mainPanel.add(lblNombre);
-txtNombre= new JTextField( 5 );						txtNombre.setBounds(410, 450, 120, 25);				mainPanel.add(txtNombre);
-lblPrimerApellido=new JLabel("Primer Apellido:");	lblPrimerApellido.setBounds(550, 450, 120, 30);		mainPanel.add(lblPrimerApellido);
-txtPrimerApellido=new JTextField(5);				txtPrimerApellido.setBounds(650, 450, 120, 25);		mainPanel.add(txtPrimerApellido);
-lblSegundoApellido=new JLabel("Segundo Apellido:");	lblSegundoApellido.setBounds(800, 450, 120, 30);	mainPanel.add(lblSegundoApellido);
-txtSegundoApellido=new JTextField(5);				txtSegundoApellido.setBounds(900, 450, 120, 25);	mainPanel.add(txtSegundoApellido);
-lblCifnif=new JLabel("Nif:");	                    lblCifnif.setBounds(1050, 450, 120, 30);            mainPanel.add(lblCifnif);
-txtCifnif=new JTextField(5);				        txtCifnif.setBounds(1100, 450, 120, 25);            mainPanel.add(txtCifnif);
-lblDireccion=new JLabel("Direccion:");				lblDireccion.setBounds(60, 500, 120, 30);			mainPanel.add(lblDireccion);
-txtDireccion=new JTextField(5);						txtDireccion.setBounds(160, 500, 120, 25);			mainPanel.add(txtDireccion);
-lblPoblacion=new JLabel("Poblacion:");				lblPoblacion.setBounds(550, 500, 120, 30);			mainPanel.add(lblPoblacion);
-txtPoblacion=new JTextField(5);						txtPoblacion.setBounds(650,500, 120, 25);			mainPanel.add(txtPoblacion);
-lblCp=new JLabel("Cp:");							lblCp.setBounds(800, 500, 120, 30);					mainPanel.add(lblCp);
-txtCp=new JTextField(5);							txtCp.setBounds(900, 500, 120, 25);					mainPanel.add(txtCp);
-lblTelefono=new JLabel("Telefono:");				lblTelefono.setBounds(300, 500, 120, 30);			mainPanel.add(lblTelefono);
-txtTelefono=new JTextField(5);						txtTelefono.setBounds(410, 500, 120, 25);			mainPanel.add(txtTelefono);
-lblSexo=new JLabel("Sexo:");						lblSexo.setBounds(1050, 500, 120, 30);				mainPanel.add(lblSexo);
-txtSexo=new JTextField(5);							txtSexo.setBounds(1100, 500, 120, 25);				mainPanel.add(txtSexo);
-lblMovil=new JLabel("Movil:");						lblMovil.setBounds(60, 550, 120, 30);				mainPanel.add(lblMovil);
-txtMovil=new JTextField(5);							txtMovil.setBounds(160, 550, 120, 25);				mainPanel.add(txtMovil);
-lblFax=new JLabel("Fax:");			   				lblFax.setBounds(300, 550, 120, 30);				mainPanel.add(lblFax);
-txtFax=new JTextField(5);							txtFax.setBounds(410, 550, 120, 25);				mainPanel.add(txtFax);
-lblEmail=new JLabel("Fax:");			   			lblEmail.setBounds(550, 550, 120, 30);				mainPanel.add(lblEmail);
-txtEmail=new JTextField(5);							txtEmail.setBounds(650, 550, 120, 25);				mainPanel.add(txtEmail);
-lblNcuenta=new JLabel("Ncuenta:");			   		lblNcuenta.setBounds(800, 550, 120, 30);			mainPanel.add(lblNcuenta);
-txtNcuenta=new JTextField(5);						txtNcuenta.setBounds(900, 550, 120, 25);			mainPanel.add(txtNcuenta);
-lblFechanacimiento=new JLabel("F.nac.:");	        lblFechanacimiento.setBounds(1050, 550, 120, 30);	mainPanel.add(lblFechanacimiento);
-txtFechanacimiento=new JTextField(5);				txtFechanacimiento.setBounds(1100, 550, 120, 25);	mainPanel.add(txtFechanacimiento);
-lblFechainicio=new JLabel("Fechainicio:");			lblFechainicio.setBounds(60, 600, 120, 30);		    mainPanel.add(lblFechainicio);
-txtFechainicio=new JTextField(5);					txtFechainicio.setBounds(160, 600, 120, 25);		mainPanel.add(txtFechainicio);
-lblComentarios=new JLabel("Comentarios:");			lblComentarios.setBounds(300, 600, 120, 30);		mainPanel.add(lblComentarios);
-txtComentarios=new JTextField(5);					txtComentarios.setBounds(410, 600, 120, 25);		mainPanel.add(txtComentarios);
 
-cmdDelete.addActionListener(this);
-cmdSetValue.addActionListener(this);
-cmdExit.addActionListener(this);
-cmdChange.addActionListener(this);
-//frame=new JFrame("Agenda de clientes");
-//frame.setSize(600,700);
-
-mainPanel.setBackground(Color.white);
-
-table.getParent().setBackground(Color.lightGray);
-//frame.getContentPane().add(mainPanel);
-//frame.setVisible(true);
+public void addColumns(String[] colName)//Table Columns
+{
+ 		for(int i=0;i<colName.length;i++)
+ 			columns.addElement((String) colName[i]);
 }
 
- public void addColumns(String[] colName)//Table Columns
- 	{
-	 		for(int i=0;i<colName.length;i++)
-	 			columns.addElement((String) colName[i]);
- 	}
-
-public void addcontent(ResultSet resultado){	
-	 try {
-         while(resultado.next()){
-             Object[] fila = new Object[16];//Creamos un Objeto con tantos parámetros como datos retorne cada fila de la consulta
-             fila[0] = resultado.getDouble("ID");
-             fila[1] = resultado.getString("NOMBRE"); //Lo que hay entre comillas son los campos de la base de datos
-             fila[2] = resultado.getString("APELLIDO1");
-             fila[3] = resultado.getString("APELLIDO2");
-             fila[4] = resultado.getString("DIRECCION");
-             fila[5] = resultado.getString("TELEFONO");
-             fila[6] = resultado.getString("POBLACION");
-             fila[7] = resultado.getString( "CP");
-             fila[8] = resultado.getString("SEXO");           
-             fila[9] = resultado.getString("MOVIL");
-             fila[10] = resultado.getString("FAX");
-             fila[11] = resultado.getString("EMAIL");
-             fila[12] = resultado.getString("NCUENTA");
-             fila[13] = resultado.getString("FECHANACIMIENTO");
-             fila[14] = resultado.getString("FECHAINICIO");
-             fila[15] = resultado.getString("COMENTARIOS");
-             tabModel.addRow(fila); // Añade una fila al final del modelo de la tabla
-         }
-         
-         table.updateUI();//Actualiza la tabla
-
-
-     } catch (Exception e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-     }
-}
 public void addRow() //Add Row
 {
 	Vector r=new Vector();
